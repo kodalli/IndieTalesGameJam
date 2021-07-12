10fd80c11946a39a73feae8c6578b8bc2c3904f6
DESKTOP-RRV4185\Kodali <surya.kodali@gmail.com>
2021-07-11 23:18:27 -0400

    sounds

--- IndieTalesGameJam2021/Assets/Scripts/Door/DoorController.cs
+++ IndieTalesGameJam2021/Assets/Scripts/Door/DoorController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour {
+    [SerializeField] private AudioClip doorSound;
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.collider.gameObject.layer == LayerMask.NameToLayer("Player")) {
             Debug.Log("Entered Door!");
+            SoundManager.Instance.PlaySound(doorSound);
             GameManager.Instance.GoToNextScene();
         }
     }
